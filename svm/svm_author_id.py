211dd2efa23281a8618868895ce9d4138eb86a06
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-05-24 17:46:02 +0100

    Went back to full training sets and got 99% accuracy and still reasonable prediction time of 20s, but training time is now 178s-- better than with no algorithm tweaks, but rather high

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,8 +26,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 clf = SVC(kernel="rbf", C=10000.0)
 
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 t0 = time()
 clf.fit(features_train, labels_train)
