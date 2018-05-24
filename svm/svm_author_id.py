be99e13bcfbbbf62efe648318257ad50cdf4a51d
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-05-24 17:27:47 +0100

    Displaying computation times for SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,19 +20,25 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
 
 from sklearn.svm import SVC
 clf = SVC(kernel="linear")
 
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
 pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
 
 from sklearn.metrics import accuracy_score
+
+to = time()
 acc = accuracy_score(pred, labels_test)
+print "accuracy calculation time:", round(time()-t0, 3), "s"
 
 print "Accuracy = ",acc
 #########################################################
