c58afdfeda038f053b0beee80920b0ea8e4d2831
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-05-24 17:32:06 +0100

    Cut down the features set to 1% of the original and got much faster training and prediction times

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,6 +26,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 clf = SVC(kernel="linear")
 
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
