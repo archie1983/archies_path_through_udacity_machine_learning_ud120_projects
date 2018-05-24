f5a9b1ba3dee10be95df8611cfa611149bb7599c
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-05-24 17:18:22 +0100

    Calculating accuracy with SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+clf = SVC(kernel="linear")
+
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print "Accuracy = ",acc
 #########################################################
-
-
