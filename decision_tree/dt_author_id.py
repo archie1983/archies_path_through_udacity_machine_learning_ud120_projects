a853509b5ba14db415bd11214a1c98b12fc6d890
Archie1983 <archie1983@hotmail.co.uk>
2018-05-27 01:53:38 +0100

    Got DT up an running and calculating accuracy with min_samples_split=40

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+t0 = time()
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+print "training time: ", round(time() - t0, 3), "s"
 
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time() - t0, 3), "s"
+
+t0 = time()
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print "accuracy determination time: ", round(time() - t0, 3), "s"
+
+print "Accuracy: ", acc
 
 #########################################################
 
