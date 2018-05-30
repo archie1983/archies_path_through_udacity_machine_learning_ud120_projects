92b24ab50421a1943b98df836954100382916649
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-05-30 17:55:13 +0100

    Showing number of email messages from Wesley Colwell to POIs

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -32,3 +32,4 @@ print "Numner of POIs: ",pois
 
 #print "Features of James Prentice: ",enron_data["PRENTICE JAMES"]
 print "Total value of the stock belonging to James Prentice: ", enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "Number of email messages from Wesley Colwell to POIs: ", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
