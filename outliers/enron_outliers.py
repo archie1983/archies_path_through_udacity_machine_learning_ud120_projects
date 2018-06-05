b6505689d0f29ee1766dc952201b0b1ed78ba889
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-06-05 15:53:17 +0100

    Printing out all persons, whose salary is greater than 1 million to find the outlier

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -11,12 +11,20 @@ from feature_format import featureFormat, targetFeatureSplit
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
+keys = data_dict.keys()
 
 
 ### your code below
+#print data_dict
+i = 0
+for key,val in data_dict.items():
+    if val['salary'] > 1000000: print key,":s:",val['salary'],":b:",val['bonus']
+
 for point in data:
     salary = point[0]
     bonus = point[1]
+#    if (salary > 1000000): print keys[i],":",salary
+#    i += 1
     matplotlib.pyplot.scatter( salary, bonus )
 
 matplotlib.pyplot.xlabel("salary")
