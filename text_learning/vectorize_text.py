89bda1ca899d2ce8e26900caf1b931f6096a4ce8
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-06-07 13:35:47 +0100

    Stemmed all emails from Chris and Sara and removed signature words

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -48,14 +48,21 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            email_text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            email_text.replace("sara", "")
+            email_text.replace("shackleton", "")
+            email_text.replace("chris", "")
+            email_text.replace("germani", "")
 
             ### append the text to word_data
+            word_data.append(email_text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if (name == "sara"): from_data.append(0)
+            if (name == "chris"): from_data.append(1)
 
             email.close()
 
@@ -66,7 +73,7 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
+print "word_data[152]: ",word_data[152]
 
 
 
