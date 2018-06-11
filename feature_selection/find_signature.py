28a5a24a43e5ae6fc17b3fb9a4f750785bbd1d25
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-06-11 12:42:42 +0100

    Added more comments to find_signature.py so that I understand, what are features here and what are the emails (data points)

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -21,17 +21,26 @@ authors = pickle.load( open(authors_file, "r") )
 ### classifier functions in versions 0.15.2 and earlier
 from sklearn import cross_validation
 features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+#print features_train
+## AE: At this point features_train is a set of stemmed, but still readable emails.
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test)
+## AE: Now we have trained the TFIDF vectoriser with a full set of training data (~90% of all emails)
+## AE: This vectoriser now has all the features, that could be gotten from 90% of all emails (that's a lot of words)
+## AE: But "features_train" now contains vectorised (transformed) emails-- ALL of them.
 
 
+#print features_train[:150]
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
 ### train on only 150 events to put ourselves in this regime
+###
+### #AE: Now we're taking only first 150 emails from the full training set (they're all vectorised at this point, btw).
+### #AE: And correspondingly first 150 labels.
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
@@ -39,6 +48,7 @@ labels_train   = labels_train[:150]
 
 ### your code goes here
 # Creating and overfitting a decision tree classifier
+### AE: We are now training a DecisionTreeClassifier with the limited number (150) of vectorised emails
 from sklearn import tree
 clf = tree.DecisionTreeClassifier()
 clf.fit(features_train, labels_train)
@@ -50,6 +60,9 @@ print "Accuracy of overfitted decision tree: ",acc
 
 # going through feature_importances_ attribute and printing out all words, that are with
 # importance of 0.2 or greater
+### AE: The words, that appear often, will, of course, be present in almost each email and therefore
+### we should have had them in the first 150 emails. We'll get the index of the most important feature
+### (word) from these 150 emails.
 i = 0
 offending_importance_index = 0
 for importance in clf.feature_importances_:
@@ -58,5 +71,7 @@ for importance in clf.feature_importances_:
         offending_importance_index = i
     i += 1
 
-# finding and printing out the data point (word) with the highest importance
-print "offending data point (word): ",vectorizer.get_feature_names()[offending_importance_index]
+# finding and printing out the feature (word) with the highest importance
+### AE: And now we'll look up this most important word in the full set of features of a vectoriser,
+### that is still fitted with the full set of emails.
+print "offending feature (word): ",vectorizer.get_feature_names()[offending_importance_index]
